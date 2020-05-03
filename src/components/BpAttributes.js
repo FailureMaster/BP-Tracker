7e1650cc4488d6a257ae9ca82fd3e309b47fe96b
FailureMaster <mike.macabulos@gmail.com>
2020-05-03 11:43:27 +0800

    Added a component that displays core Blood pressure attributes namemly the systolic, diastolic and the pulse rate. It displays the label of those core attribute with the textbox where a user can put the values that they will get on a blood pressure meter

---
+++ src/components/BpAttributes.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
+
+
+
+const BpAttributes = ({mainAttribute, subAttribute}) => {
+   
+        let [fontsLoaded] = useFonts({
+          'DS-DIGIB': require('../../assets/fonts/DS-DIGIB.ttf'),
+        });
+     
+
+     if (!fontsLoaded) {
+        return <AppLoading />;
+      }else{
+        return (
+            <View style={styles.container}>
+                <View style={styles.coreAttrib}>
+                    <Text style={styles.mainAttributeText}  >{mainAttribute}</Text>
+                    <Text>{subAttribute}</Text>
+                </View>
+                <View style = {styles.inputBoxes} >
+                    <TextInput style={styles.inputStyle} placeholder= "0 0 0"  />
+                </View>
+            </View>
+        )
+      }
+   
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: "row",
+        alignItems: "center"
+    },
+
+    coreAttrib:{
+        alignItems: "flex-end",
+        width: "40%",
+        marginVertical: 10
+    },
+    mainAttributeText:{
+        fontSize: 25,
+        textAlign:"right"
+    },
+    inputBoxes:{
+        alignContent: 'flex-end'
+    },
+    inputStyle:{
+        width: 200,
+        height: 40,
+        marginLeft: 25,
+        fontFamily: 'DS-DIGIB',
+        fontSize: 40
+    },
+    
+})
+export default BpAttributes
