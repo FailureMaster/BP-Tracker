09d4b3ffef13a87a5caab2296f9b5dcdee967719
FailureMaster <mike.macabulos@gmail.com>
2020-05-03 19:04:02 +0800

    Wrapped the whole component with KeyboardAwareScrollView component to have a functionality to scroll when keyboard is overlapping anything on the screen specially any input box

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 import BpAttribute from '../components/BpAttributes'
 
 const HomeScreen = () => {
     return (
-        <View>
-            <View style={styles.container}>
-                <Image
-                    style={styles.tinyLogo}
-                    source={require('../assets/img/bp_img.png')}
-                />
+       <KeyboardAwareScrollView>
+       
+            <View>
+                <View style={styles.container}>
+                    <Image
+                        style={styles.tinyLogo}
+                        source={require('../assets/img/bp_img.png')}
+                    />
 
+                </View>
+                <View style={styles.bpAttribute}>
+                    <BpAttribute mainAttribute="SYS" subAttribute="mmHg"/>
+                    <BpAttribute mainAttribute="DIA" subAttribute="mmHg"/>
+                    <BpAttribute mainAttribute="PULSE" subAttribute="/min"/>
+                    
+                </View>
             </View>
-            <View style={styles.bpAttribute}>
-                <BpAttribute mainAttribute="SYS" subAttribute="mmHg"/>
-                <BpAttribute mainAttribute="DIA" subAttribute="mmHg"/>
-                <BpAttribute mainAttribute="PULSE" subAttribute="/min"/>
-            </View>
-        </View>
+        </KeyboardAwareScrollView>
     )
 }
 
